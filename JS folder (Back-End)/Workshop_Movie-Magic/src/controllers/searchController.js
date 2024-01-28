6c1bf94ac61662ea6710e4d5fbf54568217dcc6f
Alex Angelvo <alexander.angelov123@gmail.com>
2024-01-28 19:58:47 +0200

    add search page

---
+++ JS folder (Back-End)/Workshop_Movie-Magic/src/controllers/searchController.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+
+router.get('/search', (req, res) => {
+
+    res.render('search');
+})
+
+
+module.exports = router;
--- JS folder (Back-End)/Workshop_Movie-Magic/src/routes.js
+++ JS folder (Back-End)/Workshop_Movie-Magic/src/routes.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const createMovieController = require('./controllers/createMovieController');
-
+const searchController = require('./controllers/searchController');
 
 router.use(homeController);
 router.use(createMovieController);
+router.use(searchController);
 
 router.get('/404', (req, res) => {
 
