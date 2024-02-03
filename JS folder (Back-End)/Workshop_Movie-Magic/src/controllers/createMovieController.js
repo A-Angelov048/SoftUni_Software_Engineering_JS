594108295e800b1689ab8bd286122caa38e9754e
Alex Angelvo <alexander.angelov123@gmail.com>
2024-02-03 22:00:44 +0200

    add the functionality of create post page

--- JS folder (Back-End)/Workshop_Movie-Magic/src/controllers/createMovieController.js
+++ JS folder (Back-End)/Workshop_Movie-Magic/src/controllers/createMovieController.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
+const { saveMovie } = require('../services/movieService');
 
 
 router.get('/create', (req, res) => {
 
     res.render('create');
+})
+
+router.post('/create', async (req, res) => {
+
+    const data = req.body;
+
+    try {
+        await saveMovie(data);
+        res.redirect('/');
+
+    } catch (error) {
+        console.log(error.message);
+        res.redirect('/create');
+    }
 })
 
 
