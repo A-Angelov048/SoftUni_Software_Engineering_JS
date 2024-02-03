3dcc91b341dbb78d876302b88a59b61acbf100f9
Alex Angelvo <alexander.angelov123@gmail.com>
2024-02-03 13:21:00 +0200

    add movie create schema

---
+++ JS folder (Back-End)/Workshop_Movie-Magic/src/models/MovieCreate.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const movieCreateSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    genre: {
+        type: String,
+        require: true
+    },
+    director: {
+        type: String,
+        require: true
+    },
+    year: {
+        type: Number,
+        require: true,
+        min: 1980,
+        max: 2030
+    },
+    imageUrl: {
+        type: String,
+        require: true
+    },
+    rating: {
+        type: Number,
+        require: true,
+        min: 1,
+        max: 5
+    },
+    description: {
+        type: String,
+        require: true
+    },
+
+})
+
+
+const Movie = mongoose.model('Movie', movieCreateSchema);
+
+module.exports = Movie;
