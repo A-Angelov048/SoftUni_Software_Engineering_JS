bab8dbea71489ce6d75ae09665f680a9be50b506
Alex Angelvo <alexander.angelov123@gmail.com>
2024-02-04 10:41:11 +0200

    add custom handlebars-helper (stars)

--- JS folder (Back-End)/Workshop_Movie-Magic/src/config/configHandlebars.js
+++ JS folder (Back-End)/Workshop_Movie-Magic/src/config/configHandlebars.js
@@ -2,10 +2,17 @@ const handlebars = require('express-handlebars');
 const path = require('path');
 
 
-function configHandlebars(app){
+function configHandlebars(app) {
 
     app.engine('hbs', handlebars.engine({
-        extname: 'hbs'
+        extname: 'hbs',
+        helpers: {
+            stars: function star(value) {
+                const star = '&#x2605;';
+                return star.repeat(value);
+            }
+
+        }
     }));
 
     app.set('view engine', 'hbs');
