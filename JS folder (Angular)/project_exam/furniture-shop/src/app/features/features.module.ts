58aff83b02f0c8426beee88f7ff15234f609fd08
Alex Angelvo <alexander.angelov123@gmail.com>
2024-03-30 22:00:34 +0200

    Add info gallery component

--- JS folder (Angular)/project_exam/furniture-shop/src/app/features/features.module.ts
+++ JS folder (Angular)/project_exam/furniture-shop/src/app/features/features.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InfoGalleryComponent } from './info-gallery/info-gallery.component';
 
 
 
 @NgModule({
   declarations: [
-    IntroductionComponent
+    IntroductionComponent,
+    AboutUsComponent,
+    InfoGalleryComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    IntroductionComponent
+    IntroductionComponent,
+    AboutUsComponent,
+    InfoGalleryComponent
   ]
 })
 export class FeaturesModule { }
---
+++ JS folder (Angular)/project_exam/furniture-shop/src/app/features/info-gallery/info-gallery.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-gallery',
+  templateUrl: './info-gallery.component.html',
+  styleUrls: ['./info-gallery.component.css']
+})
+export class InfoGalleryComponent {
+
+}
