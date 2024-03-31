4341d965ab73ab91db2535115672ef3d1cf0e961
Alex Angelvo <alexander.angelov123@gmail.com>
2024-04-01 00:01:07 +0300

    Add routes to the app
    * home router
    * 404 router

--- JS folder (Angular)/project_exam/furniture-shop/src/app/app-routing.module.ts
+++ JS folder (Angular)/project_exam/furniture-shop/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { HomeComponent } from './features/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
