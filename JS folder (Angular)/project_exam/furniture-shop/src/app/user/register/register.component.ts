61711a1949b94104f935ddf53cdaf234f32d0b48
Alex Angelvo <alexander.angelov123@gmail.com>
2024-04-05 18:23:29 +0300

    Add form logic to register component
    * added validations

--- JS folder (Angular)/project_exam/furniture-shop/src/app/user/register/register.component.ts
+++ JS folder (Angular)/project_exam/furniture-shop/src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DestroyIntroductionService } from 'src/app/shared/services/destroy-introduction.service';
 
 @Component({
@@ -9,13 +11,35 @@ import { DestroyIntroductionService } from 'src/app/shared/services/destroy-intr
 export class RegisterComponent implements OnInit, OnDestroy {
 
 
-  constructor(private introductionService: DestroyIntroductionService) { }
+  constructor(private introductionService: DestroyIntroductionService, private router: Router) { }
 
   ngOnInit(): void {
     this.introductionService.hideComponent();
   }
 
   ngOnDestroy(): void {
-    this.introductionService.hideComponent();
+    this.introductionService.showComponent();
   }
+
+
+  submitRegisterForm(form: NgForm) {
+
+    const payloadData = form.value //add type!
+
+    if (form.invalid) {
+      form.setValue(
+        {
+          username: payloadData.username,
+          email: payloadData.email,
+          password: '',
+          rePassword: ''
+        }
+      )
+      return;
+    }
+
+    // this.userService.login(payload);
+    this.router.navigate(['/']);
+  }
+
 }
