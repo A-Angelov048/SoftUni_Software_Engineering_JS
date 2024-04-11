6e92c95e108804c6016c380994e7179174ef802e
Alex Angelvo <alexander.angelov123@gmail.com>
2024-04-11 17:46:19 +0300

    Update user service
    * add http request - logout
    * add function to show and hide navbar
    * add subject and observable

--- JS folder (Angular)/project_exam/furniture-shop/src/app/shared/services/user-service.service.ts
+++ JS folder (Angular)/project_exam/furniture-shop/src/app/shared/services/user-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/types/user';
 import { environment } from 'src/environments/environment';
 
@@ -7,6 +8,10 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class UserService {
+
+  private loggedIn$$ = new BehaviorSubject<boolean>(false);
+  loggedIn$ = this.loggedIn$$.asObservable();
+
 
   constructor(private http: HttpClient) { }
 
@@ -20,4 +25,23 @@ export class UserService {
     const { apiUrl } = environment;
     return this.http.post<User>(`${apiUrl}/users/register`, data);
   }
+
+  logout() {
+    localStorage.removeItem('userId');
+    const { apiUrl } = environment;
+    return this.http.get<User>(`${apiUrl}/users/logout`);
+  }
+
+  showHideUser() {
+
+    const getUserId = localStorage.getItem('userId');
+
+    if (!!getUserId === true) {
+      this.loggedIn$$.next(true);
+    } else {
+      this.loggedIn$$.next(false);
+    }
+  }
+
+
 }
