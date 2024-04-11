72d359f87881c7b3ea95a40a958231a5d895c773
Alex Angelvo <alexander.angelov123@gmail.com>
2024-04-11 17:33:45 +0300

    Update header component
    * add logout function
    * show and hide elements in html navbar

--- JS folder (Angular)/project_exam/furniture-shop/src/app/core/header/header.component.ts
+++ JS folder (Angular)/project_exam/furniture-shop/src/app/core/header/header.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DestroyIntroductionService } from 'src/app/shared/services/destroy-introduction.service';
+import { UserService } from 'src/app/shared/services/user-service.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private userService: UserService,
+    private route: Router,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.userService.loggedIn$.subscribe(curFlag => {
+      this.isLoggedIn = curFlag;
+    })
+  }
+
+
+  logoutUser(): void {
+
+    this.userService.logout().subscribe({
+      next: () => {
+        this.userService.showHideUser();
+        this.route.navigate(['/']);
+      },
+      error: () => {
+        this.userService.showHideUser();
+        this.route.navigate(['/']);
+      }
+    }
+    )
+  }
 }
