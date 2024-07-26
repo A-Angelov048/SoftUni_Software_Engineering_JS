29f7798ad5941f2737b245406913fc14507e2bbc
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-26 08:41:15 +0300

    Add about component

--- JS (React)/project_exam/furniture-shop/src/App.jsx
+++ JS (React)/project_exam/furniture-shop/src/App.jsx
@@ -1,6 +1,7 @@
 
 import Footer from "./core-components/footer/Footer"
 import Header from "./core-components/header/Header"
+import About from "./feature-components/about/About"
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <About />
       <Footer />
     </>
   )
