216b54a6a88811654282270ad9833f632c3aaa07
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-26 11:10:00 +0300

    Update header component

--- JS (React)/project_exam/furniture-shop/src/core-components/header/Header.jsx
+++ JS (React)/project_exam/furniture-shop/src/core-components/header/Header.jsx
@@ -4,7 +4,6 @@ import './Header.css'
 
 export default function Header() {
     return (
-        <section className="introduction">
 
             <header className="header-section">
 
@@ -44,32 +43,6 @@ export default function Header() {
                 </nav>
 
             </header>
-
-            <section className="introduction-info">
-                <div className="container">
-                    <div className="row">
-                        <div className="column">
-                            <div className="detail-box">
-                                <h1>
-                                    The Latest
-                                    <p>
-                                        Furniture
-                                    </p>
-                                </h1>
-                                <p>
-                                    That brighten every room. All thoughtfully designed and meticulously crafted furniture,
-                                    for your home sweet home.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="column img-container">
-                            <div className="img-box">
-                                <img src="images/slider-img.png" alt="chair" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </section>
+            
     )
 }
