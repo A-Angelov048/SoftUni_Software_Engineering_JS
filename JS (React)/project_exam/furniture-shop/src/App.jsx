d0ff42784a21d8209df9483b136e91dd3046a064
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-26 09:57:04 +0300

    Add brand section component

--- JS (React)/project_exam/furniture-shop/src/App.jsx
+++ JS (React)/project_exam/furniture-shop/src/App.jsx
@@ -2,6 +2,7 @@
 import Footer from "./core-components/footer/Footer"
 import Header from "./core-components/header/Header"
 import About from "./feature-components/about/About"
+import BrandSection from "./feature-components/brand-section/BrandSection"
 import InfoGallery from "./feature-components/info-gallery/InfoGallery"
 
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Header />
+      <BrandSection/>
       <About />
       <InfoGallery />
       <Footer />
---
+++ JS (React)/project_exam/furniture-shop/src/feature-components/brand-section/BrandSection.jsx
@@ -0,0 +1,18 @@
+import './BrandSection.css'
+
+export default function BrandSection() {
+    return (
+        <section class="brand-section-home layout-padding">
+            <div class="container">
+                <div class="heading-container">
+                    <h2>
+                        Last Added Furniture
+                    </h2>
+                </div>
+                <a href="" class="brand-btn">
+                    See More
+                </a>
+            </div>
+        </section>
+    );
+}
