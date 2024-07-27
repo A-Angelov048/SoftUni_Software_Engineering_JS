391adba95dd21b70a78bdf6e03bc3171481b27a3
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-27 19:00:33 +0300

    Add custom hook use forms

---
+++ JS (React)/project_exam/furniture-shop/src/hooks/useForms.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+
+export function useForm(initialValues, submitCallBack) {
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandle = (e) => {
+
+        setValues(oldValue => ({
+            ...oldValue,
+            [e.target.name]: e.target.value,
+        }));
+
+    };
+
+    const submitCurForm = (e) => {
+
+        e.preventDefault();
+
+        submitCallBack(values);
+    };
+
+    return {
+        values,
+        changeHandle,
+        submitCurForm,
+    };
+}
