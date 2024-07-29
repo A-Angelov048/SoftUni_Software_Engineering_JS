4e3a4d075b2341b5f506996a3704dba1c1dcc430
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-29 23:00:25 +0300

    Add edit profile request

--- JS (React)/project_exam/furniture-shop/src/service/userService.js
+++ JS (React)/project_exam/furniture-shop/src/service/userService.js
@@ -4,4 +4,6 @@ export const register = (data) => post('/users/register', data);
 
 export const login = (data) => post('/users/login', data);
 
-export const logout = (data) => get('/users/logout', data);
+export const logout = () => get('/users/logout');
+
+export const editProfile = (data) => post('/users/edit-profile', data)
