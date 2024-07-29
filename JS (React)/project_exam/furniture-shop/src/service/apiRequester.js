a68bcf65124245890aa5453ca4127247ca51bb67
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-29 19:37:18 +0300

    Add API requester

---
+++ JS (React)/project_exam/furniture-shop/src/service/apiRequester.js
@@ -0,0 +1,52 @@
+
+const baseUrl = 'http://localhost:3000';
+
+async function request(method, url, data) {
+
+    const options = {
+        method,
+        headers: {},
+        credentials: 'include',
+    }
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    try {
+
+        const response = await fetch(baseUrl + url, options);
+
+        let result;
+
+        if (response.status !== 204) {
+            result = await response.json();
+        }
+
+        if (!response.ok) {
+            if (response.status === 403) {
+                // delete user from localStorage
+            }
+            throw result
+        }
+
+        return result;
+
+    } catch (error) {
+        return console.error(error.message);
+    }
+}
+
+export async function get(url) {
+    return await request('get', url);
+}
+export async function post(url, data) {
+    return await request('post', url, data);
+}
+export async function put(url, data) {
+    return await request('put', url, data);
+}
+export async function del(url) {
+    return await request('delete', url);
+}
