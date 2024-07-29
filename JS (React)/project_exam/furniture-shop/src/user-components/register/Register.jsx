315c6dd31f8a6c616a7f400f39e7bdc2b7649076
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-29 19:40:54 +0300

    Add register logic

--- JS (React)/project_exam/furniture-shop/src/user-components/register/Register.jsx
+++ JS (React)/project_exam/furniture-shop/src/user-components/register/Register.jsx
@@ -1,7 +1,37 @@
 import '../UserForms.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { useForm } from '../../hooks/useForms';
+import { register } from '../../service/userService';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+
+const initialValues = {
+    username: '',
+    email: '',
+    password: '',
+    rePassword: ''
+};
 
 export default function Register() {
+
+    const navigate = useNavigate();
+    const { changeAuthState } = useContext(AuthContext);
+
+    const createUser = async (values) => {
+
+        try {
+            const result = await register(values);
+            changeAuthState(result);
+            navigate('/');
+        } catch (error) {
+            console.error(error.message);
+        }
+
+    }
+
+    const { values, changeHandler, submitCurForm } = useForm(initialValues, createUser);
+
     return (
         <section className="register layout-padding">
             <div className="container">
@@ -12,22 +42,50 @@ export default function Register() {
                 </div>
                 <div className="layout-padding2">
                     <div className="wrapper-user">
-                        <form>
+                        <form onSubmit={submitCurForm}>
                             <div className="input-box">
-                                <input type="text" placeholder="Type your username*" name="username" />
+                                <input
+
+                                    type="text"
+                                    placeholder="Type your username*"
+                                    name="username"
+                                    value={values.username}
+                                    onChange={changeHandler} />
+
                                 <i className='bx bxs-user'></i>
                             </div>
 
                             <div className="input-box">
-                                <input type="email" placeholder="Type your email*" name="email" />
+                                <input
+
+                                    type="email"
+                                    placeholder="Type your email*"
+                                    name="email"
+                                    value={values.email}
+                                    onChange={changeHandler} />
+
                                 <i className='bx bxs-envelope'></i>
                             </div>
                             <div className="input-box">
-                                <input type="password" placeholder="Type your password*" name="password" />
+                                <input
+
+                                    type="password"
+                                    placeholder="Type your password*"
+                                    name="password"
+                                    value={values.password}
+                                    onChange={changeHandler} />
+
                                 <i className='bx bxs-lock-alt'></i>
                             </div>
                             <div className="input-box">
-                                <input type="password" placeholder="Confirm your password*" name="rePassword" />
+                                <input
+
+                                    type="password"
+                                    placeholder="Confirm your password*"
+                                    name="rePassword"
+                                    value={values.rePassword}
+                                    onChange={changeHandler} />
+
                                 <i className='bx bxs-lock-alt'></i>
                             </div>
 
