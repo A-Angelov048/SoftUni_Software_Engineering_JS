76e0b9b88d108845fea0cbbde92bf6c1162b275d
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-29 22:55:28 +0300

    Add change user info logic

--- JS (React)/project_exam/furniture-shop/src/user-components/profile/settings/Settings.jsx
+++ JS (React)/project_exam/furniture-shop/src/user-components/profile/settings/Settings.jsx
@@ -1,23 +1,66 @@
 import './Settings.css'
+import { useContext } from 'react';
+
+import { useForm } from '../../../hooks/useForms';
+import { editProfile } from '../../../service/userService';
+import { AuthContext } from '../../../context/AuthContext';
 
 export default function Settings() {
+
+    const { imageProfile, username, location, changeAuthState } = useContext(AuthContext);
+
+    const initialValues = {
+        imageProfile: imageProfile,
+        username: username,
+        location: location,
+    }
+
+    const changeUserInfo = async (values) => {
+        const result = await editProfile(values);
+        changeAuthState(result);
+    }
+
+
+    const { values, changeHandler, submitCurForm } = useForm(initialValues, changeUserInfo)
+
     return (
         <div className="settings layout-padding2">
 
             <details>
                 <summary>Profile editing</summary>
-                <form>
+                <form onSubmit={submitCurForm}>
 
                     <div className="input-box">
-                        <input type="text" name="location" placeholder="City or post code *" />
+
+                        <input type="text"
+                            name="username"
+                            placeholder="Username *"
+                            value={values.username}
+                            onChange={changeHandler}
+                        />
+
                     </div>
 
                     <div className="input-box">
-                        <input type="text" name="username" placeholder="Username *" />
+
+                        <input type="text"
+                            name="location"
+                            placeholder="City or post code"
+                            value={values.location}
+                            onChange={changeHandler}
+                        />
+
                     </div>
 
                     <div className="input-box">
-                        <input type="email" name="email" placeholder="Email *" />
+
+                        <input type="text"
+                            name="imageProfile"
+                            placeholder="Profile image"
+                            value={values.imageProfile}
+                            onChange={changeHandler}
+                        />
+
                     </div>
 
                     <button type="submit" className="btn">Save</button>
