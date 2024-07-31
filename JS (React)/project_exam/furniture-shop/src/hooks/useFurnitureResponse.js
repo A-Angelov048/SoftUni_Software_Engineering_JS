ad42525ed4f25bd1d055a116f53e156d07cb7c35
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-07-31 21:36:11 +0300

    Add all furniture

--- JS (React)/project_exam/furniture-shop/src/hooks/useFurnitureResponse.js
+++ JS (React)/project_exam/furniture-shop/src/hooks/useFurnitureResponse.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { getLatestFurniture } from "../service/furnitureService";
+import { getAllFurniture, getLatestFurniture } from "../service/furnitureService";
 import { useSetFurniture } from "./useFurnitureReducer";
 
 
@@ -14,6 +14,31 @@ export function latestFurniture() {
             try {
 
                 const response = await getLatestFurniture();
+                dispatch({ type: 'GET_FURNITURE', payload: response });
+
+            } catch (error) {
+                console.error(error.message);
+            }
+
+        })();
+
+    }, []);
+
+    return furniture;
+
+}
+
+export function allFurniture() {
+
+    const [furniture, dispatch] = useSetFurniture();
+
+    useEffect(() => {
+
+        (async () => {
+
+            try {
+
+                const response = await getAllFurniture();
                 dispatch({ type: 'GET_FURNITURE', payload: response });
 
             } catch (error) {
