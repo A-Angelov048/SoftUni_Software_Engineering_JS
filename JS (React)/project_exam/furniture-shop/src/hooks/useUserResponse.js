988030a5a89a18a52b0ec63528af7af1f9d4e0ed
Alex Angelvo <alexander.angelov123@gmail.com>
2024-08-07 19:10:26 +0300

    Add user response
    * add hook use get profile

---
+++ JS (React)/project_exam/furniture-shop/src/hooks/useUserResponse.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { getProfile } from "../service/userService";
+
+export function useGetProfile(params) {
+
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+
+        const abortController = new AbortController();
+
+        (async () => {
+
+            try {
+
+                const response = await getProfile(params, abortController);
+                setUser(response);
+
+            } catch (error) {
+                console.error(error.message);
+            }
+
+        })();
+
+        return () => {
+            abortController.abort();
+        }
+
+    }, []);
+
+    return user;
+
+}
