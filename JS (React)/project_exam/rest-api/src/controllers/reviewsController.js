c301d7ede897b447788629e119be877968d59c27
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-03 23:05:16 +0300

    Add reviews requester controller

---
+++ JS (React)/project_exam/rest-api/src/controllers/reviewsController.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { createReview } = require('../services/reviewsService');
+
+
+router.post('/save-review/:id', async (req, res) => {
+
+    const body = req.body;
+    const owner = {
+        _id: req.user._id,
+        username: req.user.username,
+        imageProfile: req.user.imageProfile
+    }
+    const furnitureId = req.params.id;
+
+    try {
+
+        const result = await createReview({ ...body, owner }, furnitureId);
+
+        res.json(result);
+
+    } catch (err) {
+        console.log(err.errors);
+    }
+})
+
+module.exports = router;
