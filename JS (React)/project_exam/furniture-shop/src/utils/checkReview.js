7ab74d726b0192f54bc150e22afe82e3ae2f351c
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-03 23:30:53 +0300

    Add condition for review

---
+++ JS (React)/project_exam/furniture-shop/src/utils/checkReview.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { FurnitureContext } from "../context/FurnitureContext";
+import { AuthContext } from "../context/AuthContext";
+
+export function checkReview() {
+
+    const user = useContext(AuthContext);
+    const furnitureContext = useContext(FurnitureContext);
+
+    let flag = false;
+
+    if (furnitureContext.owner?._id !== user.userId && user.userId !== undefined) {
+        flag = true;
+    }
+
+    furnitureContext.reviews?.forEach(x => {
+        if (x.owner._id === user.userId) {
+            flag = false;
+        }
+    })
+
+    return flag;
+}
