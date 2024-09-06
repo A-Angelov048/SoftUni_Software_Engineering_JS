88a0a114d8712fc47d3eeca52159f1526bc79b06
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-06 23:41:50 +0300

    Add del furniture function
    * add close function
    * update CSS

--- JS (React)/project_exam/furniture-shop/src/shared-components/message-dialog/MessageDialog.jsx
+++ JS (React)/project_exam/furniture-shop/src/shared-components/message-dialog/MessageDialog.jsx
@@ -1,21 +1,37 @@
 import "./MessageDialog.css";
+import { useNavigate, useParams } from 'react-router-dom';
+import { removeFurniture } from "../../service/furnitureService";
 
-export default function MessageDialog() {
-    
+export default function MessageDialog({ onClose }) {
+
+    const navigate = useNavigate();
+    const { furnitureId } = useParams();
+
+    async function deleteFurniture() {
+        
+        try {
+            await removeFurniture(furnitureId);
+            navigate('/shop');
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+
     return (
-        <div className="overlay">
+        <div className="overlay" onClick={onClose}>
             <div className="content">
 
                 <header className="header-content" >
                     <h2>Message dialog</h2>
-                    <i class='bx bx-x' ></i>
+                    <i class='bx bx-x' onClick={onClose}></i>
                 </header>
 
                 <p>Are you sure you want to delete this furniture?</p>
 
                 <div className="button-box">
-                    <button name="delete" type="button" className="btn-hover-delete">Delete</button>
-                    <button name="cancel" type="button" className="btn-hover-cancel">Cancel</button>
+                    <button onClick={deleteFurniture} name="delete" type="button" className="btn-hover-delete">Delete</button>
+                    <button onClick={onClose} name="cancel" type="button" className="btn-hover-cancel">Cancel</button>
                 </div>
 
             </div>
