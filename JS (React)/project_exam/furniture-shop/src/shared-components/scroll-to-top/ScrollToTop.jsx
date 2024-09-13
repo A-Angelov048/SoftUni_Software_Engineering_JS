b2602d82ab07bfd3995681bbc5030e91dc2759b2
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-13 23:41:58 +0300

    Add error state and componet

--- JS (React)/project_exam/furniture-shop/src/shared-components/scroll-to-top/ScrollToTop.jsx
+++ JS (React)/project_exam/furniture-shop/src/shared-components/scroll-to-top/ScrollToTop.jsx
@@ -1,16 +1,20 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import ErrorDialog from "../error-dialog/ErrorDialog";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function ScrollToTop(props) {
 
     const { pathname } = useLocation();
+    const { error } = useContext(AuthContext);
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
 
     return <>
+        {error && <ErrorDialog />}
         {props.children}
     </>
-    
+
 }
