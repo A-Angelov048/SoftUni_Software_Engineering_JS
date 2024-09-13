7c01b74b2f757227e06da5d77ec09b5f77519f5b
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-13 23:40:51 +0300

    Add error dialog component

---
+++ JS (React)/project_exam/furniture-shop/src/shared-components/error-dialog/ErrorDialog.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+
+export default function ErrorDialog() {
+
+    const navigate = useNavigate();
+    const { changeAuthState, updateError } = useContext(AuthContext);
+
+    function onClose() {
+        changeAuthState({});
+        updateError(false);
+        navigate('/login');
+    }
+
+    return (
+        <div className="overlay">
+            <div className="content">
+
+                <header className="header-content" >
+                    <h2>Error dialog</h2>
+                </header>
+
+                <p>Session expired. Please log in again.</p>
+
+                <div className="button-box">
+                    <button onClick={onClose} name="ok" type="button" className="btn-hover">Ok</button>
+                </div>
+
+            </div>
+        </div>
+    );
+}
