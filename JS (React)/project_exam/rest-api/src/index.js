f6bd14901948a42218e36172bf0dd44c08758af6
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-13 22:37:17 +0300

    Remove handlebars  config

--- JS (React)/project_exam/rest-api/src/index.js
+++ JS (React)/project_exam/rest-api/src/index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-
 
 const expressConfig = require('./config/expressConfig');
-const handlebarsConfig = require('./config/handlebarsConfig');
 const router = require('./router');
-
 
 const app = express();
 const port = 3000;
 
 expressConfig(app);
-handlebarsConfig(app);
-
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(router);
 
 mongoose.connect('mongodb://localhost:27017/furniture-shop')
