176153cd944d7f4a2ec8332457de514fb652107d
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-13 22:37:44 +0300

    Update express config

--- JS (React)/project_exam/rest-api/src/config/expressConfig.js
+++ JS (React)/project_exam/rest-api/src/config/expressConfig.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const {auth} = require('../middlewares/authMiddleware');
+const { auth } = require('../middlewares/authMiddleware');
+const cors = require('cors');
 
 
 function expressConfig(app) {
 
+    app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
