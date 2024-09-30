3ccf6b50489ec818a2781a4a1a7b40a32726f3e8
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-09-30 22:41:22 +0300

    Fix scroll on some components

--- JS folder (React)/project_exam/furniture-shop/src/shared-components/scroll-to-top/ScrollToTop.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/shared-components/scroll-to-top/ScrollToTop.jsx
@@ -5,10 +5,16 @@ import { AuthContext } from "../../context/AuthContext";
 
 export default function ScrollToTop(props) {
 
+    const withoutScrollArr = ['wishlist', 'settings', 'my-furniture'];
     const { pathname } = useLocation();
     const { error } = useContext(AuthContext);
 
     useEffect(() => {
+
+        const checkPathname = pathname.split('/').pop();
+
+        if (withoutScrollArr.includes(checkPathname)) return;
+
         window.scrollTo(0, 0);
     }, [pathname]);
 
