82afaac9e7fad3956f25b11ff88c23faee407f42
Alexander Angelvo <alexander.angelov123@gmail.com>
2024-12-01 23:58:35 +0200

    Clear forms at first render

--- JS folder (React)/project_exam/furniture-shop/src/shared-components/scroll-to-top/ScrollToTop.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/shared-components/scroll-to-top/ScrollToTop.jsx
@@ -2,15 +2,19 @@ import { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ErrorDialog from "../error-dialog/ErrorDialog";
 import { AuthContext } from "../../context/AuthContext";
+import { ErrorContext } from "../../context/ErrorContext";
 
 export default function ScrollToTop(props) {
 
     const withoutScrollArr = ['wishlist', 'settings', 'my-furniture'];
     const { pathname } = useLocation();
     const { error } = useContext(AuthContext);
+    const { clearError } = useContext(ErrorContext);
 
     useEffect(() => {
 
+        clearError();
+        
         const checkPathname = pathname.split('/').pop();
 
         if (withoutScrollArr.includes(checkPathname)) return;
