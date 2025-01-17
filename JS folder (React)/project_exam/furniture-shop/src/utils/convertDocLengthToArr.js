12789ed80eb685999c012dcb275c822a4eadfb82
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-01-17 14:40:25 +0200

    Add convert document length to array

---
+++ JS folder (React)/project_exam/furniture-shop/src/utils/convertDocLengthToArr.js
@@ -0,0 +1,12 @@
+export function convertDocLengthToArr(lengthDocuments) {
+
+    const pages = Math.ceil(lengthDocuments / 8);
+
+    const arrayPages = [];
+
+    for (let i = 1; i <= pages; i++) {
+        arrayPages.push(i);
+    }
+
+    return arrayPages;
+}
