e28378f19d88fff269044e9433b0f78fead64380
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-01-18 19:19:09 +0200

    Add basket component

---
+++ JS folder (React)/project_exam/furniture-shop/src/user-components/basket/Basket.jsx
@@ -0,0 +1,17 @@
+export default function Basket() {
+
+    return (
+        <section className="register layout-padding">
+            <div className="container">
+                <div className="heading-container">
+                    <h2>
+                        Basket
+                    </h2>
+                </div>
+                <div className="layout-padding2">
+                    
+                </div>
+            </div>
+        </section>
+    );
+}
