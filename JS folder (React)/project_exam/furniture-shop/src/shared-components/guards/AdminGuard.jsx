a278d76907117fb543696731d32ccc6d69e9f3a8
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-01-23 23:03:59 +0200

    Add admin guard

---
+++ JS folder (React)/project_exam/furniture-shop/src/shared-components/guards/AdminGuard.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+
+export default function AdminGuard() {
+
+    const { pathname } = useLocation();
+    const { role } = useContext(AuthContext);
+
+    return pathname.split('/').pop() === role.toLowerCase() ? <Outlet /> : <Navigate to={'/404'} replace={true} />
+
+}
