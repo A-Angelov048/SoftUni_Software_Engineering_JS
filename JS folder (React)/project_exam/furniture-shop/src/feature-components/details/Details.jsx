5366ac2a4d286e3bd7de5d434e75d7dc56108b56
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-01-25 23:55:29 +0200

    Use basket context

--- JS folder (React)/project_exam/furniture-shop/src/feature-components/details/Details.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/feature-components/details/Details.jsx
@@ -9,6 +9,7 @@ import { AuthContext } from '../../context/AuthContext';
 import { FurnitureContext } from '../../context/FurnitureContext';
 import { averageNumReviews } from '../../utils/averageNumReviews';
 import MessageDialog from '../../shared-components/message-dialog/MessageDialog';
+import { BasketContext } from '../../context/BasketContext';
 
 export default function Details() {
 
@@ -24,6 +25,7 @@ export default function Details() {
 
     const { userId, role } = useContext(AuthContext);
     const { reviews, listUserLikes } = useContext(FurnitureContext);
+    const { changeBasketState } = useContext(BasketContext);
 
     const ref = useRef(null);
 
@@ -202,7 +204,7 @@ export default function Details() {
                                                 :
 
                                                 <>
-                                                    <button disabled={quantity <= 0 ? true : false} onClick={null} className={quantity > 0 ? 'btn-hover' : 'btn-hover blur'} name="add-to-cart" type="button">Add to Basket</button>
+                                                    <button disabled={quantity <= 0 ? true : false} onClick={() => changeBasketState({ id: furnitureId, quantity: quantity })} className={quantity > 0 ? 'btn-hover' : 'btn-hover blur'} name="add-to-cart" type="button">Add to Basket</button>
                                                     <button name="heart" type="button">
                                                         <i onClick={() => updateWishlist(furnitureId)} className={listUserLikes?.includes(userId) ? 'bx bxs-heart bx-tada-hover active' : 'bx bxs-heart bx-tada-hover'}></i>
                                                     </button>
