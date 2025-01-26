47599553edcb1658ae11b1d48e1f8cc26b5b70ff
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-01-26 18:51:52 +0200

    Fix change basket state logic

--- JS folder (React)/project_exam/furniture-shop/src/context/BasketContext.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/context/BasketContext.jsx
@@ -8,7 +8,18 @@ export function BasketProvider(props) {
     const [basketState, setBasketState] = usePersistence([], 'basket');
 
     const changeBasketState = (state) => {
-        setBasketState(oldState => [...oldState, state]);
+
+        const newBasket = [...basketState];
+
+        const findCurrent = newBasket.find(x => x.id === state.id)
+
+        if (findCurrent) {
+            findCurrent.quantity = state.quantity;
+        } else {
+            newBasket.push(state);
+        }
+
+        setBasketState(newBasket);
     };
 
     const removeBasketState = (id) => {
