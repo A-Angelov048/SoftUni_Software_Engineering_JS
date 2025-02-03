a8664a37d88c192a50f4105c346d66afed6af402
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-03 22:57:42 +0200

    Update & Add two new functions

--- JS folder (React)/project_exam/furniture-shop/src/context/BasketContext.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/context/BasketContext.jsx
@@ -1,17 +1,19 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import usePersistence from "../hooks/usePersistence";
+import { ErrorContext } from "./ErrorContext";
 
 export const BasketContext = createContext();
 
 export function BasketProvider(props) {
 
     const [basketState, setBasketState] = usePersistence([], 'basket');
+    const { handleError, clearError } = useContext(ErrorContext);
 
     const changeBasketState = (state) => {
 
         const newBasket = [...basketState];
 
-        const findCurrent = newBasket.find(x => x.id === state.id)
+        const findCurrent = newBasket.find(x => x.id === state.id);
 
         if (findCurrent) {
             findCurrent.quantity = state.quantity;
@@ -23,16 +25,56 @@ export function BasketProvider(props) {
     };
 
     const removeBasketState = (id) => {
-        
+
         const newBasket = [...basketState];
 
         setBasketState(newBasket.filter(item => item.id !== id));
+
+        handleError({ successMessage: 'Successfully remove item from basket.' });
+
+        setTimeout(() => {
+
+            clearError();
+
+        }, 2000);
     };
+
+    const quantityHandler = (idFurniture, operation) => {
+
+        const findCurrent = basketState.find(x => x.id === idFurniture);
+
+        switch (operation) {
+
+            case 'increment': if (findCurrent.quantity >= 10) { return } findCurrent.quantity++; break;
+
+            case 'decrement': if (findCurrent.quantity <= 1) { return } findCurrent.quantity--; break;
+
+        }
+
+        changeBasketState({ id: findCurrent.id, quantity: findCurrent.quantity });
+
+    }
+
+    const addToBasket = (furnitureId, quantity, price) => {
+        
+        changeBasketState({ id: furnitureId, quantity: quantity, price: price });
+
+        handleError({ successMessage: 'Successfully add furniture to basket.' });
+
+        setTimeout(() => {
+
+            clearError();
+
+        }, 2000);
+    };
+
 
     const data = {
         basketItems: basketState,
         changeBasketState,
         removeBasketState,
+        quantityHandler,
+        addToBasket,
     };
 
 
