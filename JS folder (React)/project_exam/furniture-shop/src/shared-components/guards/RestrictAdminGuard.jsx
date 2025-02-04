a8e6cce5d4d6986b6a8d301696b3775e0f606f92
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-04 22:46:28 +0200

    Add restrict admin guard component

---
+++ JS folder (React)/project_exam/furniture-shop/src/shared-components/guards/RestrictAdminGuard.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function RestrictAdminGuard() {
+
+    const { role } = useContext(AuthContext);
+
+    return role !== 'Admin' ? <Outlet /> : <Navigate to={'/404'} replace={true} />;
+
+}
