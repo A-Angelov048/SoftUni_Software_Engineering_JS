af87d4fd8ab227b58cea5f843d4be6de95de6f25
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-07 16:57:21 +0200

    Update add parameter to function

--- JS folder (React)/project_exam/furniture-shop/src/utils/priceHandler.js
+++ JS folder (React)/project_exam/furniture-shop/src/utils/priceHandler.js
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { BasketContext } from "../context/BasketContext";
 
-export function priceHandler() {
+export function priceHandler(priceReceive) {
 
 
     const { basketItems } = useContext(BasketContext);
 
     const totalPrice = basketItems.reduce((acc, initValue) => acc + (initValue.price * initValue.quantity), 0);
 
-    return totalPrice;
+    return totalPrice + priceReceive;
 
 }
