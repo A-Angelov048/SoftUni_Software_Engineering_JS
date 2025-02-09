48c7ec34835830e520bd9daa93fec52ce80fd4f3
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-09 19:13:39 +0200

    Add dynamic form
    *add validation for inputs
    *add error handler for inputs
    *add submit for form

--- JS folder (React)/project_exam/furniture-shop/src/user-components/deliveryForm/DeliveryForm.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/user-components/deliveryForm/DeliveryForm.jsx
@@ -1,19 +1,86 @@
 import './DeliveryForm.css';
+import { useContext } from 'react';
+import { useForm } from '../../hooks/useForms';
+import { ErrorContext } from '../../context/ErrorContext';
+import { AuthContext } from '../../context/AuthContext';
+import { deliveryFormSchema } from '../../utils/schemaForm';
 
 
 
 export default function DeliveryForm() {
+
+    const { errors, handleError, clearError } = useContext(ErrorContext);
+    const { userId, role, changeAuthState, updateAuthError } = useContext(AuthContext);
+
+
+    const createDeliveryInfo = async (values) => {
+        
+        try {
+            await deliveryFormSchema.validate(values, { abortEarly: false });
+        } catch (error) {
+
+            const newError = {};
+
+            error.inner.forEach((err) => {
+                newError[err.path] = err.message;
+            })
+
+            handleError(newError);
+
+            return;
+        }
+
+        try {
+
+        } catch (error) {
+
+            if (error.message === '403') return updateAuthError(true);
+
+            console.error(error.message);
+
+        }
+    }
+
+    const { values, changeHandler, submitCurForm } = useForm({}, createDeliveryInfo);
+
+
     return (
-        <form className='delivery-form'>
+        <form onSubmit={submitCurForm} className='delivery-form'>
 
             <div className='row-input'>
 
                 <div className="form-group">
-                    <input type="text" id="last-name" placeholder="Last name*" />
+                    <input
+                        type="text"
+                        name="last-name"
+                        placeholder="Last name*"
+                        value={values['last-name']}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('last-name') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors['last-name']}</p>
+                        </div>
+                    }
                 </div>
 
+
+
                 <div className="form-group">
-                    <input type="text" id="first-name" placeholder="Name*" />
+                    <input
+                        type="text"
+                        name="first-name"
+                        placeholder="Name*"
+                        value={values['first-name']}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('first-name') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors['first-name']}</p>
+                        </div>
+                    }
                 </div>
 
             </div>
@@ -21,11 +88,35 @@ export default function DeliveryForm() {
             <div className='row-input'>
 
                 <div className="form-group">
-                    <input type="text" id="address" placeholder="Address*" />
+                    <input
+                        type="text"
+                        name="address"
+                        placeholder="Address*"
+                        value={values.address}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('address') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors.address}</p>
+                        </div>
+                    }
                 </div>
 
                 <div className="form-group">
-                    <input type="text" id="zip-code" placeholder="Zip code*" />
+                    <input
+                        type="text"
+                        name="zip-code"
+                        placeholder="Zip code*"
+                        value={values['zip-code']}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('zip-code') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors['zip-code']}</p>
+                        </div>
+                    }
                 </div>
 
             </div>
@@ -33,11 +124,35 @@ export default function DeliveryForm() {
             <div className="row-input">
 
                 <div className="form-group">
-                    <input type="text" id="region" placeholder="Region*" />
+                    <input
+                        type="text"
+                        name="region"
+                        placeholder="Region*"
+                        value={values.region}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('region') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors.region}</p>
+                        </div>
+                    }
                 </div>
 
                 <div className="form-group">
-                    <input type="text" id="city" placeholder="City / District*" />
+                    <input
+                        type="text"
+                        name="city"
+                        placeholder="City / District*"
+                        value={values.city}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('city') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors.city}</p>
+                        </div>
+                    }
                 </div>
 
             </div>
@@ -45,17 +160,53 @@ export default function DeliveryForm() {
             <div className="row-input">
 
                 <div className="form-group">
-                    <input type="text" id="district" placeholder="Neighborhood*" />
+                    <input
+                        type="text"
+                        name="neighborhood"
+                        placeholder="Neighborhood*"
+                        value={values.neighborhood}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('neighborhood') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors.neighborhood}</p>
+                        </div>
+                    }
                 </div>
 
                 <div className="form-group">
-                    <input type="email" id="email" placeholder="Email*" />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email*"
+                        value={values.email}
+                        onChange={changeHandler}
+                    />
+                    {errors.hasOwnProperty('email') &&
+                        <div className='error-container'>
+                            <i className='bx bxs-error-circle bx-tada' ></i>
+                            <p className='error'>{errors.email}</p>
+                        </div>
+                    }
                 </div>
 
             </div>
 
             <div className="form-group">
-                <input type="tel" id="phone" placeholder="Phone number*" />
+                <input
+                    type="text"
+                    name="phone"
+                    placeholder="Phone number*"
+                    value={values.phone}
+                    onChange={changeHandler}
+                />
+                {errors.hasOwnProperty('phone') &&
+                    <div className='error-container'>
+                        <i className='bx bxs-error-circle bx-tada' ></i>
+                        <p className='error'>{errors.phone}</p>
+                    </div>
+                }
             </div>
 
             <button type="submit" className="save-btn">Save</button>
