f2586bcad72102ef34b05dac0b112f8dc3df0b6c
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-14 23:41:57 +0200

    Add basket markup

--- JS folder (React)/project_exam/furniture-shop/src/user-components/basket/checkout/Checkout.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/user-components/basket/checkout/Checkout.jsx
@@ -3,6 +3,7 @@ import { useGetDeliveryInfo } from '../../../hooks/useUserResponse';
 import DeliveryForm from '../../delivery-form/DeliveryForm';
 import { useContext, useState } from 'react';
 import { ErrorContext } from '../../../context/ErrorContext';
+import { Link } from 'react-router-dom';
 
 export default function Checkout() {
 
@@ -13,7 +14,7 @@ export default function Checkout() {
     const deliveryInfo = useGetDeliveryInfo();
 
     return (
-        <section className="basket-page layout-padding">
+        <section className="checkout-page layout-padding">
 
             <div className="container">
 
@@ -30,7 +31,7 @@ export default function Checkout() {
                     </div>
                 }
 
-                <div className="layout-padding2">
+                <div className="layout-padding2 checkout">
 
                     <div className="wrapper-checkout">
 
@@ -46,9 +47,10 @@ export default function Checkout() {
 
                             {deliveryInfo.hasOwnProperty('_id') && changeInfo ?
                                 <div className='delivery-info'>
-                                    <p>{deliveryInfo['first-name']} {deliveryInfo['last-name']}</p>
-                                    <p>{deliveryInfo.address}, {deliveryInfo['zip-code']}</p>
-                                    <p>{deliveryInfo.city}, {deliveryInfo.region}</p>
+                                    <p>{`${deliveryInfo['first-name']} ${deliveryInfo['last-name']}`}</p>
+                                    <p>{`${deliveryInfo.address}, ${deliveryInfo['zip-code']}`}</p>
+                                    <p>{deliveryInfo.neighborhood}</p>
+                                    <p>{`${deliveryInfo.city}, ${deliveryInfo.region}`}</p>
                                     <p>{deliveryInfo.email}</p>
                                     <p>{deliveryInfo.phone}</p>
                                 </div>
@@ -59,6 +61,34 @@ export default function Checkout() {
                         </div>
 
                     </div >
+
+                    <div class="basket">
+                        <div class="basket-header">
+                            <h3>Basket</h3>
+                            <Link className='link' to={'/basket'}>Back to basket</Link>
+                        </div>
+                        <div class="basket-items">
+                            <img src="image.png" alt="Product 1" />
+                            <img src="image.png" alt="Product 2" />
+                        </div>
+                        <div class="basket-summary">
+                            <div class="summary-row">
+                                <span>Products:</span>
+                                <span>$640,00</span>
+                            </div>
+                            <div class="summary-row">
+                                <span>Shipping:</span>
+                                <span>$25,00</span>
+                            </div>
+                            <div class="summary-row total">
+                                <span>Total amount:</span>
+                                <span>$665,00</span>
+                            </div>
+                            <div className='low-font'>
+                                <span>(including VAT)</span>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
 
