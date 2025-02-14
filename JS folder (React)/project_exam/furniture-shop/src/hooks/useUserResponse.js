45b0fc8857da6a4abbd4b980e7f42aa97c967721
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-14 19:33:42 +0200

    Add function use get delivery info

--- JS folder (React)/project_exam/furniture-shop/src/hooks/useUserResponse.js
+++ JS folder (React)/project_exam/furniture-shop/src/hooks/useUserResponse.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
-import { getProfile } from "../api-service/userService";
+import { getDeliveryInfo, getProfile } from "../api-service/userService";
 import { AuthContext } from "../context/AuthContext";
+import { useForm } from "./useForms";
+import { useLocation } from "react-router-dom";
 
 export function useGetProfile(profileId) {
 
@@ -33,5 +35,39 @@ export function useGetProfile(profileId) {
     }, [profileId, userContext]);
 
     return user;
+
+}
+
+export function useGetDeliveryInfo() {
+
+    const location = useLocation();
+    const userContext = useContext(AuthContext);
+    const [info, setInfo] = useState({});
+
+    useEffect(() => {
+
+        const abortController = new AbortController();
+
+        (async () => {
+
+            try {
+
+                const response = await getDeliveryInfo();
+                setInfo(response);
+            } catch (error) {
+                if (error.message === '403') return userContext.updateAuthError(true);
+
+                console.error(error.message);
+            }
+
+        })();
+
+        return () => {
+            abortController.abort();
+        }
+
+    }, [location]);
+
+    return info;
 
 }
