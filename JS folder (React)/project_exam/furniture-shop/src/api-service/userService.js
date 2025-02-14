1dd52329da7735c365ecb78727d38ea9257a7cbb
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-14 19:35:02 +0200

    Add request functions for BE

--- JS folder (React)/project_exam/furniture-shop/src/api-service/userService.js
+++ JS folder (React)/project_exam/furniture-shop/src/api-service/userService.js
@@ -6,6 +6,10 @@ export const login = (data) => post('/users/login', data);
 
 export const logout = () => get('/users/logout');
 
-export const getProfile = (userId, abortController) => get(`/users/profile/${userId}`, abortController)
+export const getProfile = (userId, abortController) => get(`/users/profile/${userId}`, abortController);
 
-export const editProfile = (data) => post('/users/edit-profile', data)
+export const editProfile = (data) => post('/users/edit-profile', data);
+
+export const createDeliveryInfo = (data) => post('/users/delivery-info', data);
+
+export const getDeliveryInfo = (data) => get('/users/delivery-info', data);
