d53e231f75593b20bcd858d09ff097854bb0a59c
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-15 00:40:37 +0200

    Remove parameter

--- JS folder (React)/project_exam/furniture-shop/src/utils/priceHandler.js
+++ JS folder (React)/project_exam/furniture-shop/src/utils/priceHandler.js
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { BasketContext } from "../context/BasketContext";
 
-export function priceHandler(priceReceive) {
+export function priceHandler() {
 
 
     const { basketItems } = useContext(BasketContext);
 
     const totalPrice = basketItems.reduce((acc, initValue) => acc + (initValue.price * initValue.quantity), 0);
 
-    return (totalPrice + priceReceive).toFixed(2);
+    return totalPrice;
 
 }
