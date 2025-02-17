095f241beba4becb4f47585c85527d42a637a0a6
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-02-17 23:10:31 +0200

    Create submit button component

---
+++ JS folder (React)/project_exam/furniture-shop/src/shared-components/submit-button/SubmitButton.jsx
@@ -0,0 +1,11 @@
+import styles from './SubmitButton.module.css';
+
+export default function SubmitButton({ disabledButton = false, clickFunction, buttonName = 'Submit' }) {
+    return (
+        <button disabled={disabledButton} onClick={clickFunction}
+            type='button'
+            className={styles['sub-btn']}>
+            {buttonName}
+        </button >
+    );
+}
