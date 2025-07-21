3318f827c78e4114e53f4bc12db55f0ad7b4274d
Alexander Angelvo <alexander.angelov123@gmail.com>
2025-07-21 22:30:22 +0300

    Add shop pages to browser history

--- JS folder (React)/project_exam/furniture-shop/src/feature-components/shop/Shop.jsx
+++ JS folder (React)/project_exam/furniture-shop/src/feature-components/shop/Shop.jsx
@@ -1,15 +1,24 @@
-import BrandContainer from "../../shared-components/brand-container/BrandContainer";
-import Spinner from "../../shared-components/spinner/Spinner";
-
+import { useEffect } from "react";
 import { useAllFurniture } from "../../hooks/useFurnitureResponse";
-import { convertDocLengthToArr } from "../../utils/convertDocLengthToArr";
 import { useHandlePage } from "../../hooks/useHandlePage";
+import { convertDocLengthToArr } from "../../utils/convertDocLengthToArr";
+import { useLocation, useSearchParams } from "react-router-dom";
 import Pagination from "../../shared-components/pagination/Pagination";
+import Spinner from "../../shared-components/spinner/Spinner";
+import BrandContainer from "../../shared-components/brand-container/BrandContainer";
 
 export default function Shop() {
-  const [statePage, handlePageChange] = useHandlePage(1);
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+  const [statePage, handlePageChange] = useHandlePage(
+    Number(searchParams.get("page")) || 1
+  );
   const [furniture, lengthDocuments] = useAllFurniture(statePage);
   const lengthPages = convertDocLengthToArr(lengthDocuments, 8);
+
+  useEffect(() => {
+    window.history.replaceState(null, "", `/shop/?page=${statePage}`);
+  }, [statePage, location]);
 
   return (
     <section className="brand-section layout-padding">
